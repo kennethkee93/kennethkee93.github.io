0fd4d3cc62b7da37497fc3e6be8c8cfbc9c64289
kennethkee93 <kennethkjw@outlook.com>
2025-10-15 08:49:03 +0000

    commit

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'emerald-hills-ev-cache-V2.0.13'; // Incrementing version to ensure update
+const CACHE_NAME = 'emerald-hills-ev-cache-V2.0.14'; // Incrementing version to ensure update
 
 // A list of critical static files (the "App Shell") to cache on install
 const urlsToCache = [
