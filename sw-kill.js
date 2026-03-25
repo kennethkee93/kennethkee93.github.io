cb6da2f56357db7a163c86d5eda9e3da81748495
kennethkee93 <kennethkjw@outlook.com>
2026-03-25 16:32:04 +0800

    Update service worker to clear cache and unregister

--- sw-kill.js
+++ sw-kill.js
@@ -1,21 +1,34 @@
-/* sw-kill.js - Clears PWA cache and unregisters */
-self.addEventListener('install', () => {
-  self.skipWaiting(); 
+// We use a new version name to trigger the browser's update check
+const CACHE_NAME = 'emerald-hills-ev-SHUTDOWN-APRIL-1'; 
+
+self.addEventListener('install', event => {
+  console.log('SW: Shutting down app...');
+  // Force this new "kill" worker to take over immediately
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => caches.delete(key)));
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          console.log('SW: Deleting cache:', cacheName);
+          return caches.delete(cacheName);
+        })
+      );
     }).then(() => {
-      return self.clients.claim();
-    }).then(() => {
+      // Unregister itself so it doesn't run again
       return self.registration.unregister();
     }).then(() => {
-      // Force any open tabs to reload to the new index.html
-      return self.clients.matchAll().then((clients) => {
+      // Force all open tabs to reload to see the "Moved" HTML
+      return self.clients.matchAll().then(clients => {
         clients.forEach(client => client.navigate('/'));
       });
     })
   );
+});
+
+// Disable all fetching logic to ensure nothing is served from cache
+self.addEventListener('fetch', event => {
+  return; 
 });
